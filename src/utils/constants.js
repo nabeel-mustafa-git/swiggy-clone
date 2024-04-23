886ecc7281e72fc254d74b5bc51f1a266d113821
Nabeel Mustafa <nabeelmustafaskp@gmail.com>
2024-04-23 23:19:16 +0500

    api url changed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,12 +2,13 @@
 
 export const corsBypasser = "https://corsproxy.io/?";
 
-export const apiUrlRestaurants = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING";
+export const apiUrlRestaurants =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+//  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING";
 
 export const apiUrlRestaurantMenu =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
-
-// "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.572646&lng=88.36389500000001&restaurantId=";
 
 export const cloudinaryImageUrl = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
